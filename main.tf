ee30cfd808088c418eb2f4457f1fa1f965b9d192
AppElent <ericjansen@live.nl>
2022-08-04 07:08:02 +0000

    resource group creation

--- main.tf
+++ main.tf
@@ -1,3 +1,37 @@
+
 variable "environment" {
   default = "dev"
+  description = "Environment name"
 }
+
+location = "West Europe"
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+
+
+resource "azurerm_resource_group" "rg" {
+  location = "${var.location}"
+  name     = "rg-azure-infra-${var.environment}"
+
+}
+
+# # Create a resource group
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources"
+#   location = "West Europe"
+# }
