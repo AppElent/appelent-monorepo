afa30a19810d9b43e3e720e7658696adc78aeaa6
Eric Jansen <ericjansen@live.nl>
2023-01-28 13:57:32 +0100

    terraform updates

--- infrastructure/common_providers.tf
+++ infrastructure/common_providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = ">= 3.41.0"
     }
     github = {
       source  = "integrations/github"
