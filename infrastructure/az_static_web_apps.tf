28c601112af24bcf4668884088c374d676dd3e65
Eric Jansen <ericjansen@live.nl>
2023-01-29 14:21:02 +0100

    remove static web apps

--- infrastructure/az_static_web_apps.tf
+++ infrastructure/az_static_web_apps.tf
@@ -12,30 +12,30 @@ module "az_static_web_app" {
   dns_ttl            = 300
 }
 
-module "az_static_web_app_satisfactory" {
-  source = "./modules/az_static_web_app"
+# module "az_static_web_app_satisfactory" {
+#   source = "./modules/az_static_web_app"
 
-  static_web_app_name = "swa-appelent-satisfactory"
-  resource_group_name = azurerm_resource_group.rg_swa.name
-  location            = var.location
-  sku                 = "Free"
+#   static_web_app_name = "swa-appelent-satisfactory"
+#   resource_group_name = azurerm_resource_group.rg_swa.name
+#   location            = var.location
+#   sku                 = "Free"
 
-  custom_domain_name = "satisfactory"
-  dns_rg             = azurerm_resource_group.rg_networking.name
-  dns_zone_name      = azurerm_dns_zone.appelent.name
-  dns_ttl            = 300
-}
+#   custom_domain_name = "satisfactory"
+#   dns_rg             = azurerm_resource_group.rg_networking.name
+#   dns_zone_name      = azurerm_dns_zone.appelent.name
+#   dns_ttl            = 300
+# }
 
-module "az_static_web_app_demo" {
-  source = "./modules/az_static_web_app"
+# module "az_static_web_app_demo" {
+#   source = "./modules/az_static_web_app"
 
-  static_web_app_name = "swa-appelent-demo"
-  resource_group_name = azurerm_resource_group.rg_swa.name
-  location            = var.location
-  sku                 = "Free"
+#   static_web_app_name = "swa-appelent-demo"
+#   resource_group_name = azurerm_resource_group.rg_swa.name
+#   location            = var.location
+#   sku                 = "Free"
 
-  custom_domain_name = "demo"
-  dns_rg             = azurerm_resource_group.rg_networking.name
-  dns_zone_name      = azurerm_dns_zone.appelent.name
-  dns_ttl            = 300
-}
+#   custom_domain_name = "demo"
+#   dns_rg             = azurerm_resource_group.rg_networking.name
+#   dns_zone_name      = azurerm_dns_zone.appelent.name
+#   dns_ttl            = 300
+# }
