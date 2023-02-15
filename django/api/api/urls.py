479114840b83427c2711606ca4b0fc8ea83ce3c7
AppElent <ericjansen@live.nl>
2023-02-15 18:17:47 +0100

    production ready django api

--- django/api/api/urls.py
+++ django/api/api/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,5 +24,7 @@ urlpatterns = [
     path('oauth/', include('oauth.urls')),
     path('crud/', include('crud.urls')),
     path('oauth/', include('oauth.urls')),
+    path('users/', include('users.urls')),
+    path('', views.dummy, name='dummy'),
     re_path(r'^health/', include('health_check.urls')),
 ]
--- django/api/users/views.py
+++ django/api/users/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import  Response
+from rest_framework.views import APIView
+from .serializers import RegistrationSerializer, PasswordChangeSerializer
 # Create your views here.
+
+
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request):
+        serializer = PasswordChangeSerializer(context={'request': request}, data=request.data)
+        serializer.is_valid(raise_exception=True) #Another way to write is as in Line 17
+        request.user.set_password(serializer.validated_data['new_password'])
+        request.user.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
